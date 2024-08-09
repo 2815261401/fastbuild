507480eaaadb3bd4c542c0cd18830a41d921d3f7
李成豪 <2815261401@qq.com>
2024-08-09 15:13:43 +0800

    build: 🔧更新eslint,prettier的配置 分支: master

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -3,45 +3,48 @@
  * 在本文件中修改格式化规则，不会同时触发改变ESLint代码检查，所以每次修改本文件需要重启VSCode，ESLint检查才能同步代码格式化
  * 需要相应的代码格式化规范请自行查阅配置，下面为默认项目配置
  */
-module.exports = {
-  // 在单独的箭头函数参数周围包括括号 always：(x) => x \ avoid：x => x
-  arrowParens: 'always',
-  // 在对象文字中的括号之间打印空格
-  bracketSpacing: true,
-  //在 windows 操作系统中换行符通常是回车 (CR) 加换行分隔符 (LF)，也就是回车换行(CRLF)，
-  //然而在 Linux 和 Unix 中只使用简单的换行分隔符 (LF)。
-  //对应的控制字符为 "\n" (LF) 和 "\r\n"(CRLF)。auto意为保持现有的行尾
-  // 换行符使用 lf 结尾是 可选值"<auto|lf|crlf|cr>"
-  endOfLine: 'lf',
-  // 指定HTML文件的全局空格敏感度 css\strict\ignore
-  htmlWhitespaceSensitivity: 'css',
-  // 不需要自动在文件开头插入 @prettier
-  insertPragma: false,
-  // 在JSX中使用单引号而不是双引号
+/** @type {import('prettier').Config} **/
+const config = {
+  /** 用制表符而不是空格缩进行 */
+  useTabs: false,
+  /** 指定每个缩进级别的空格数 */
+  tabWidth: 2,
+  /** 句末使用分号 */
+  semi: true,
+  /** 使用单引号 */
+  singleQuote: true,
+  /** jsx 使用双引号 */
   jsxSingleQuote: false,
-  // jsx 标签的反尖括号需要换行
-  jsxBracketSameLine: false,
-  //是否自动组织import
-  organizeImports: true,
-  // 一行最多多少个字符
-  printWidth: 120,
-  // 使用默认的折行标准 always\never\preserve
-  proseWrap: 'preserve',
-  // 更改引用对象属性的时间 可选值"<as-needed|consistent|preserve>"
-  quoteProps: 'as-needed',
-  // 这两个选项可用于格式化以给定字符偏移量（分别包括和不包括）开始和结束的代码
+  /** 尽可能的打印末尾的逗号 */
+  trailingComma: 'all',
+  /** 打印对象字面量中括号之间的空格 */
+  bracketSpacing: true,
+  /** 将>放在末尾而不是另起一行 */
+  bracketSameLine: false,
+  /** 这两个选项可用于格式化以给定字符偏移量（分别包括和不包括）开始和结束的代码 */
   rangeStart: 0,
   rangeEnd: Infinity,
-  // 指定要使用的解析器，不需要写文件开头的 @prettier
+  /** 指定要使用的解析器，不需要写文件开头的 @prettier */
   requirePragma: false,
-  // 在语句末尾是否需要分号
-  semi: true,
-  // 是否使用单引号
-  singleQuote: true,
-  // 指定每个缩进级别的空格数
-  tabWidth: 2,
-  // 多行时尽可能打印尾随逗号。（例如，单行数组永远不会出现逗号结尾。） 可选值"<none|es5|all>"，默认none
-  trailingComma: 'es5',
-  // 使用制表符而不是空格缩进行
-  useTabs: false,
+  /** 使用默认的折行标准 always\never\preserve */
+  proseWrap: 'preserve',
+  /** 在唯一的箭头函数参数周围包括括号 always：(x) => x \ avoid：x => x */
+  arrowParens: 'always',
+  /** 如何处理HTML中的空白 */
+  htmlWhitespaceSensitivity: 'css',
+  /** 使用lf作为换行符 */
+  endOfLine: 'lf',
+  /** 当对象中的属性被引用时更改 */
+  quoteProps: 'as-needed',
+  /** 是否缩进Vue文件中<script>和<style>标签内的代码 */
+  vueIndentScriptAndStyle: false,
+  /** 控制Prettier是否格式化文件中嵌入的引号代码 */
+  embeddedLanguageFormatting: 'auto',
+  /** 在HTML、Vue和JSX中每行执行一个属性 */
+  singleAttributePerLine: true,
+  /** 使用三元表达式，在条件后面加上问号，而不是和结果在同一行 */
+  experimentalTernaries: false,
+  /** jsx中将>放在末尾而不是另起一行 */
+  jsxBracketSameLine: false,
 };
+module.exports = config;
