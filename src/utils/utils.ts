60c4dbce6eb5aedfcfb1be8896efa0f7168416ec
李成豪 <2815261401@qq.com>
2025-07-14 14:14:38 +0800

    chore: 🔥移除未使用的函数

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,21 +1,7 @@
 import { existsSync, readFileSync } from 'node:fs'
-import { createRequire } from 'node:module'
 import path from 'node:path'
 
 import { parse } from 'jsonc-parser'
-
-/**
- * 导入js文件
- * @param filePath 文件路径
- * @returns 导入的数据
- */
-export function requireFile<T = any>(filePath: string): T {
-  const require = createRequire(import.meta.url)
-  const key = require.resolve(filePath)
-  delete require.cache[key]
-  const result = require(filePath)
-  return result.default ? result.default : result
-}
 
 /** 读取json */
 export function readJson<T = any>(jsonPath: string): T | null {
